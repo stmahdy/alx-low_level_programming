c483f92d4f965688cdb3f85215a1791e43ce41fb
stmahdy <statimahdi@outlook.com>
2023-06-20 16:03:54 -0700

    v5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * _isalpha -  checks for alphabetic character
- * Returns 1 if c is a letter, lowercase or uppercase
- * Returns 0 otherwise
- *
- * **/
+ * _isalpha - tests whether a character is from the English alphabet.
+ * @c: character to be checked.
+ * Return: 1 if the character is an English character.
+ * 0 if the character is not an English character.
+ */
 int _isalpha(int c)
-{
-	
+{	
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
