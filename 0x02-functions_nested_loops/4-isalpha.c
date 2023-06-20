2eedcdf93fba6aab946bd6de15bc1928364270b9
stmahdy <statimahdi@outlook.com>
2023-06-20 16:09:45 -0700

    v6

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,8 +5,7 @@
  * Return: 1 if the character is an English character.
  * 0 if the character is not an English character.
  */
-int _isalpha(int c)
-{	
+int _isalpha(int c){	
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
