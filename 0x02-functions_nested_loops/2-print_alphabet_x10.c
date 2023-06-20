49fe11e2685ae698a48f589a1f4ccca7dd1bd2da
stmahdy <statimahdi@outlook.com>
2023-06-20 14:22:18 -0700

    first test for 2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ * Return: 0 on success.
+ */
+
+void print_alphabet_x10(void)
+{
+	int temp;
+	char let;
+
+	for (temp = 0 ; temp <= 10 ; temp++)
+		for (let = 'a' ; let <=  'z' ; let ++)
+			_putchar(let);
+
+	_putchar('\n');
+}
