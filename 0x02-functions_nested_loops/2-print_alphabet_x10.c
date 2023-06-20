5b0581794f40e63a456de769398c361a6bf057da
stmahdy <statimahdi@outlook.com>
2023-06-20 15:14:04 -0700

    v3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 	{
 		for (let = 'a' ; let <=  'z' ; let++)
 		{
-                        _putchar(let);
+                           _putchar(let);
 
 		}
 		_putchar('\n');
